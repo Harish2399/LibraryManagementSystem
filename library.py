e4d878e98afef8bba4e97e2b7f15ee226b52fd6d
Harish Ragavendra <harishragavendra9240@gmail.com>
2026-06-25 00:27:14 +0530

    Version 2 - Added Book ID and Author

--- library.py
+++ library.py
@@ -6,24 +6,34 @@ if not os.path.exists(BOOK_FILE):
     open(BOOK_FILE, "w").close()
 
 def add_book():
-    book = input("Enter book name: ")
+    book_id = input("Enter Book ID: ")
+    book_name = input("Enter Book Name: ")
+    author = input("Enter Author Name: ")
+
     with open(BOOK_FILE, "a") as f:
-        f.write(book + "\n")
-    print("Book added successfully!")
+        f.write(f"{book_id},{book_name},{author}\n")
+
+    print("Book Added Successfully!")
 
 def view_books():
     with open(BOOK_FILE, "r") as f:
         books = f.readlines()
 
-    if len(books) == 0:
-        print("No books available.")
-    else:
-        print("\nAvailable Books:")
-        for i, book in enumerate(books, start=1):
-            print(f"{i}. {book.strip()}")
+    if not books:
+        print("No books available")
+        return
+
+    print("\nBOOK LIST")
+    print("-" * 50)
+    print("ID\tBook Name\tAuthor")
+    print("-" * 50)
+
+    for book in books:
+        data = book.strip().split(",")
+        print(f"{data[0]}\t{data[1]}\t{data[2]}")
 
 def search_book():
-    name = input("Enter book name to search: ")
+    search_id = input("Enter Book ID: ")
 
     with open(BOOK_FILE, "r") as f:
         books = f.readlines()
@@ -31,25 +41,32 @@ def search_book():
     found = False
 
     for book in books:
-        if name.lower() in book.lower():
-            print("Book Found:", book.strip())
+        data = book.strip().split(",")
+
+        if data[0] == search_id:
+            print("\nBook Found")
+            print("Book ID:", data[0])
+            print("Book Name:", data[1])
+            print("Author:", data[2])
             found = True
 
     if not found:
-        print("Book not found.")
+        print("Book Not Found")
 
 def delete_book():
-    name = input("Enter book name to delete: ")
+    delete_id = input("Enter Book ID to delete: ")
 
     with open(BOOK_FILE, "r") as f:
         books = f.readlines()
 
     with open(BOOK_FILE, "w") as f:
         for book in books:
-            if book.strip().lower() != name.lower():
+            data = book.strip().split(",")
+
+            if data[0] != delete_id:
                 f.write(book)
 
-    print("Book deleted if it existed.")
+    print("Book Deleted Successfully")
 
 while True:
     print("\n===== LIBRARY MANAGEMENT SYSTEM =====")
@@ -59,7 +76,7 @@ while True:
     print("4. Delete Book")
     print("5. Exit")
 
-    choice = input("Enter choice: ")
+    choice = input("Enter Choice: ")
 
     if choice == "1":
         add_book()
@@ -74,8 +91,8 @@ while True:
         delete_book()
 
     elif choice == "5":
-        print("Thank you!")
+        print("Thank You")
         break
 
     else:
-        print("Invalid choice.")
+        print("Invalid Choice")
